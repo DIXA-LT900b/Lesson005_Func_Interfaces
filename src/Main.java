821a2bf7383020dcfcd9371efb0b99067b4ef258
DIXA <dixa@xaker.ru>
2023-09-05 13:16:42 +0300

    Созданы все нужные классы. Ошибка деления на ноль устранена.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        System.out.println("Переменная а = " + a);
+
+        int b = calc.minus.apply(1,1);
+        System.out.println("Переменная b = " + b);
+
+        try {
+            int c = calc.devide.apply(a, b);
+            System.out.println("Переменная c = " + c);
+        } catch ( ArithmeticException e) {
+            System.out.println("Невозможно рассчитать переменную с (Деление на ноль)!");
+        }
     }
 }
